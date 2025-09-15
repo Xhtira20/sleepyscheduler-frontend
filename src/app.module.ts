cfb4c3e8ff245164a1243da603733ec38e730443
htira-abderrazak <abderrazak.htira@gmail.com>
2025-09-15 14:44:57 +0100

    fix : creating the user tables by registered via TypeOrmModule.forFeature() inside a module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
@@ -23,7 +24,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true, // Auto-create DB schema (not for production)
         logging: true,
       }),
-    }),
+     }),
+      UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.module.ts
@@ -0,0 +1,9 @@
+// src/users/users.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+})
+export class UsersModule {}
